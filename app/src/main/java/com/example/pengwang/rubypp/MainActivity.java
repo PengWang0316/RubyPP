d2e8c5ee586f315742c02a2421cbc3213c464544
Peng <rocwang@cityuniversity.edu>
2016-12-15 21:57:08 -0800

    Fix action bar and recycler view

--- app/src/main/java/com/example/pengwang/rubypp/MainActivity.java
+++ app/src/main/java/com/example/pengwang/rubypp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.pengwang.rubypp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mainRecyclerView;
+    private LinearLayoutManager mainLinerLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting action bar
+        Toolbar myToolBar = (Toolbar)findViewById(R.id.maim_tool_bar);
+        setSupportActionBar(myToolBar);
+
+        //Setting up the recycler view
+        mainRecyclerView=(RecyclerView) findViewById(R.id.main_recycler_view);
+        mainLinerLayoutManager = new LinearLayoutManager(this);
+        mainRecyclerView.setLayoutManager(mainLinerLayoutManager);
+
     }
 }
